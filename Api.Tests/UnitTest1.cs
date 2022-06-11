822bb6dfde7e1b8321c4fee1eb57143ab0f89305
raulmesteves94 <raul.m.esteves@gmail.com>
2022-06-11 19:19:36 -0300

    Ajuste nos projetos de teste

--- Api.Tests/UnitTest1.cs
+++ Api.Tests/UnitTest1.cs
@@ -1,11 +1,15 @@
+using Api2.Controllers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
 namespace Api.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Get_OnSucess_returnStatusCode200()
         {
-
+            var config = new ConfigurationBuilder();
         }
     }
 }
